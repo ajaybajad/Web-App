c8aa24906126fa2f4701ef238c5cb1e66feb0c59
Ajay Bajad <111170054+ajaybajad@users.noreply.github.com>
2022-12-16 12:08:21 +0530

    Add files via upload

---
+++ main_activity.java
@@ -0,0 +1,31 @@
+package com.example.webapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        web = findViewById(R.id.web);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new callback());
+        web.loadUrl("https://www.google.com");
+
+    }
+
+    private class callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) { return false; }
+    }
